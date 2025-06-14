9b6fac1738173295cdd2060d739411d4db4f6a96
aniket30i <iamaniket.saha.09@protonmail.com>
2025-06-15 01:03:01 +0530

    hero content texts

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="text-slate-800">
+    <div className="text-slate-800 flex flex-col justify-center items-center pt-12">
       <h1 className="text-6xl">Materials you can put to work</h1>
+      <p className="p-10 text-center text-lg">
+        From science experiments to grammar guides, discover resources that turn
+        screen time into learning time. <br />
+        Curated for parents who want more than just content—they want impact.
+      </p>
     </div>
   );
 };
