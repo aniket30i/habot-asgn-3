ed9ca31b0a1176aadcfaa7ac7fe00da6f3b6a3e3
aniket30i <iamaniket.saha.09@protonmail.com>
2025-06-15 00:29:36 +0530

    addition of dummy data and font import

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
-import { useState } from "react";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <>Habot assignment 3</>;
+  return <div>Assignment</div>;
 }
 
 export default App;
