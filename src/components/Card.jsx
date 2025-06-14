625c54826053dd24fe9b296bddb05dfd74e4df7a
aniket30i <iamaniket.saha.09@protonmail.com>
2025-06-15 02:33:02 +0530

    like functionality , favicon and title

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import liked from "../assets/icons/liked.png";
+import unliked from "../assets/icons/unliked.png";
 
 const Card = ({
   id,
@@ -9,7 +11,16 @@ const Card = ({
   type,
   category,
 }) => {
-  console.log("liked status:", isLiked);
+  const [likes, setLikes] = useState([]);
+
+  const handleLike = (id) => {
+    if (likes.includes(id)) {
+      setLikes(likes.filter((id) => id !== id));
+    } else {
+      setLikes([...likes, id]);
+    }
+  };
+
   return (
     <div key={id}>
       <div className=" bg-slate-100 rounded-xl">
@@ -19,10 +30,16 @@ const Card = ({
         <div className="p-5">
           <label>{category}</label>
           <p className="font-semibold text-xl my-2">{title}</p>
-          <div className="mt-10 flex justify-between">
+          <div className="mt-10 flex justify-between items-center">
             <label>{type}</label>
             <p>time : {readTime} mins</p>
-            <p>{isLiked}</p>
+            <span onClick={() => handleLike(id)}>
+              {isLiked || likes.includes(id) ? (
+                <img src={liked} className="h-5 cursor-pointer" />
+              ) : (
+                <img src={unliked} className="h-5 cursor-pointer" />
+              )}
+            </span>
           </div>
         </div>
       </div>
